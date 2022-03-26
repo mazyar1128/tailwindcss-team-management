48cd37065cf66a945809207c13d2d86319b329c7
mazyar1128 <mazyar1128@yahoo.com>
2022-03-26 22:00:17 +0430

    First commit, dashboard designed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '.65rem'
+      },
+      colors: {
+       
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat','sans-serif'],
+        'poppins': ['Poppins','sans-serif'],
+        'inter': ['Inter','sans-serif'],
+        
+   
+      }
+      
+    },
+  },
+  plugins: [],
+}
